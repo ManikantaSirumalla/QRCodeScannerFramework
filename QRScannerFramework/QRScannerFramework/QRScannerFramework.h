5759a2ac302a7000fb79a897afe61c3c06d172e4
Manikanta Sirumalla <Manikanta.sirumalla@icloud.com>
2023-08-26 01:23:20 +0530

    add framework

---
+++ QRScannerFramework/QRScannerFramework/QRScannerFramework.h
@@ -0,0 +1,18 @@
+//
+//  QRScannerFramework.h
+//  QRScannerFramework
+//
+//  Created by Manikanta Sirumalla on 26/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QRScannerFramework.
+FOUNDATION_EXPORT double QRScannerFrameworkVersionNumber;
+
+//! Project version string for QRScannerFramework.
+FOUNDATION_EXPORT const unsigned char QRScannerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRScannerFramework/PublicHeader.h>
+
+
